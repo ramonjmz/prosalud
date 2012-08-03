900fa79e01c7a3ec02028495d1a2524987358b9e
Betobaz <pedroalberto007@gmail.com>
2012-08-03 01:08:48 -0500

    filtros por test en analysis/update

--- public/js/controllers/ResultsController.js
+++ public/js/controllers/ResultsController.js
@@ -1,6 +1,6 @@
 Prosalud.controllers.results = Prosalud.controllers.items || {};
 
-Prosalud.controllers.results.collectionController = Em.ArrayController.create({
+Prosalud.controllers.results.dataController = Em.ArrayController.create({
 	content: [],
     store : new Store({
         name: "results_store",
@@ -8,12 +8,17 @@ Prosalud.controllers.results.collectionController = Em.ArrayController.create({
     }),
     search: function(query, callback){
     	var self = this
-    		resultCollection = [];
+    		resultCollection = [],
+            store = self.get("store");
 
-    	self.get("store").search(query)
+        store.url = "/results/list-json";
+    	store.search(query)
     		.done(function(data){
     			if(data.result && data.result.length > 0){
     				$.map(data.result, function(item, index){
+                        if(item.test_id){
+                            item.test_id = parseInt(item.test_id);
+                        }
     					resultCollection.push(Prosalud.models.Result.create(item));
     				});
     			}
@@ -23,6 +28,15 @@ Prosalud.controllers.results.collectionController = Em.ArrayController.create({
     		.fail(function(){
     			callback && callback();
     		})
+    }
+
+});
+
+Prosalud.controllers.results.collectionController = Em.ArrayController.create({
+    content: [],
+
+    filterBy: function(key, value) {
+        this.set('content', Prosalud.controllers.results.dataController.filterProperty(key, value));
     }
 
 });
--- public/js/views/Tests/ListFilterResultView.js
+++ public/js/views/Tests/ListFilterResultView.js
@@ -5,8 +5,12 @@ Prosalud.views.tests.ListFilterResultView = Ember.View.extend({
 	tagName: "tr",
 
 	filterResult: function(){
-		var test = this.get( 'test' );
-		console.log( test );
-	}
+		var test = this.get( 'test' );		
+		Prosalud.controllers.results.collectionController.filterBy("test_id", test.get("id"));
+	},
+
+	showAll: function(){
+        Prosalud.controllers.results.collectionController.set('content', Prosalud.controllers.results.dataController.get( "content" ));
+    }
 
 });
