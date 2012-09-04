d5f0488ffec5a04f2e0f4ef8eb47a455c8e766f1
ramonjmz <ramonjmx@gmail.com>
2012-09-04 03:35:47 -0500

    nota de remision

--- application/controllers/PaymentsController.php
+++ application/controllers/PaymentsController.php
@@ -3,7 +3,7 @@
 class PaymentsController extends Zend_Controller_Action
 {
 
-	 
+
 	public function init(){
 
 
@@ -15,44 +15,54 @@ class PaymentsController extends Zend_Controller_Action
 	}
 
 	public function detailAction(){
-		 
+			
 	}
 
 	public function addAction(){
-		 
+			
 		if (!$this->_hasParam('id')) {
 			return $this->_redirect('/analysis/index/page/1');
-			
+
 		}
 
 		$convert = new Application_Model_Letras();
-		
+
 		$model = new Application_Model_Payments();
-				
+
 		$datos = new Application_Model_Analysis();
 
 		$row = $datos->getRow($this->_getParam('id'));
 		if($row){
 
 			$data =$row->toArray();
+
+			$contact = new Application_Model_Contacts();
+				
+			$customer = $contact->getRow($data['applicant_id'])->toArray();
+			
+			$paciente = $customer['first_name'].' '.$customer['last_name'];
+
+			 
 			$exa =$datos->BySpecialties($this->_getParam('id'));
 			$monto =0;
- 			foreach($exa as $key) {
-		 
- 				 $monto = $monto +$key['amount'];
+			foreach($exa as $key) {
+					
+				$monto = $monto +$key['amount'];
 			}
-			
+
 			$model->save();
-			
+
 
 			$this->view->paginator = $exa;
 
 			$this->view->letras =$convert->num2letras($monto) ;
+			$this->view->paciente =$paciente;
+			$this->view->data=$data;
 			$this->view->monto = $monto;
-			
+
 
 		}
-		 
+			
 	}
 }
 
